5692e53b7a147c9737b595b73f750415d33859e2
Matthieu CLIN <matthieu.clin@wanadoo.fr>
2025-05-05 10:40:56 +0200

    add: link UI with viewmodel

--- src/app/pages/home/game.viewmodel.ts
+++ src/app/pages/home/game.viewmodel.ts
@@ -1,6 +1,7 @@
 
 
 export enum GameState {
+    IN_PROGRESS,
     WIN,
     LOSE
 }
@@ -13,4 +14,42 @@ export type Guess = {
 export type GameVM = {
     state: GameState
     guesses: Guess[]
+}
+
+export class GameViewModel {
+
+    constructor() { }
+
+    newGame(): GameVM {
+        return {
+            state: GameState.IN_PROGRESS,
+            guesses: [
+                {
+                    userGuess: 10,
+                    response: "Trop grand"
+                }, {
+                    userGuess: 5,
+                    response: "Trop petit"
+                }
+            ]
+        };
+    }
+
+    guess(userGuess: number): GameVM {
+        return {
+            state: GameState.WIN,
+            guesses: [
+                {
+                    userGuess: 10,
+                    response: "Trop grand"
+                }, {
+                    userGuess: 5,
+                    response: "Trop petit"
+                }, {
+                    userGuess: userGuess,
+                    response: "Trouvé !"
+                }
+            ]
+        };
+    }
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,12 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from "@angular/material/button";
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from "@angular/material/icon";
 import { MatInputModule } from "@angular/material/input";
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { GameState, GameVM } from './game.viewmodel';
+import { GameState, GameViewModel, GameVM } from './game.viewmodel';
 
 @Component({
     selector: 'home-root',
@@ -14,25 +14,25 @@ import { GameState, GameVM } from './game.viewmodel';
     styleUrl: './home.component.scss',
     imports: [MatToolbarModule, CommonModule, MatCardModule, FormsModule, MatInputModule, MatButtonModule, MatIconModule]
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
   
-  game: GameVM = {
-    state: GameState.WIN,
-    guesses: [
-      {
-        userGuess: 10,
-        response: "Trop grand"
-      }, {
-        userGuess: 5,
-        response: "Trop petit"
-      }, {
-        userGuess: 8,
-        response: "Trouvé !"
-      }
-    ]
-  };
+  gameViewModel: GameViewModel;
 
+  game!: GameVM;
 
-  constructor() {}
+  input: String = "";
+
+  constructor() {
+    this.gameViewModel = new GameViewModel();
+  }
+
+  ngOnInit(): void {
+      this.game = this.gameViewModel.newGame();
+  }
+
+  submit(): void {
+    this.game = this.gameViewModel.guess(Number(this.input));
+    this.input = "";
+  }
 
 }
